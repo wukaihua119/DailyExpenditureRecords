860331a58be332355399fc0fb145a1ce769d5bb1
wukaihua119 <37163606+wukaihua119@users.noreply.github.com>
2020-04-02 22:34:43 +0800

    Update OUT.h

--- DailyExpenditureRecords/src/c/libs/OUT.h
+++ DailyExpenditureRecords/src/c/libs/OUT.h
@@ -10,7 +10,8 @@ int D_trans_fee;
 void OUT_output( char * );
 int calexpense( size_t cal_expense, size_t cal_Ccash );
 int calbalance( size_t cal_Dcash, size_t cal_Ccash,size_t cal_balance );
-void caldetail( void ); 
+void caldetail( char *Ditem, size_t Dcash ); 
+void getdetail( void ); 
 void out_to_file( void ); /* build redirecting output function */ 
 
 #endif
