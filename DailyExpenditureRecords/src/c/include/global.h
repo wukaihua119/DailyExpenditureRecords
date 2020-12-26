4cd3994b141229166a7fc636049069c780ee52b0
wukaihua <tvjs168@gmail.com>
2020-12-26 23:45:31 +0800

    Decoupling the program.

---
+++ DailyExpenditureRecords/src/c/include/global.h
@@ -0,0 +1,26 @@
+#ifndef GLOBAL_H_
+#define GLOBAL_H_ 
+
+#include<stdio.h> 
+
+#define OUTPUT output
+#define INPUT input 
+
+/* define a struct */
+typedef struct{
+    size_t date;
+    char Ditem[10];
+    char Citem[10];
+    char ref[10];
+    size_t Dcash;
+    size_t Ccash;
+    size_t total_expense;
+    size_t balance;
+    size_t T_eating; 
+    size_t T_transfee; 
+    size_t T_expense; 
+}detail;
+
+
+#endif 
+
